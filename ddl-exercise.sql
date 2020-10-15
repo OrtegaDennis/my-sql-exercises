d219b3672ad5a0fdffb2687b95876dc302512435
Dennis Ortega <dortega2@wisc.edu>
2020-10-15 15:45:13 -0500

    ddl exercise

---
+++ ddl-exercise.sql
@@ -0,0 +1,24 @@
+drop database if exists ddl_exercise_music_catalog;
+
+create database field_agent;
+
+create table artist(
+ agency_id int primary key auto_increment
+);
+
+create table album(
+);
+
+create table track();
+
+
+
+
+
+
+
+
+
+
+
+
